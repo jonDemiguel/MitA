73928a9d3a3dc153b1f9e28235783153536b5b0a
Guanlin Dai <Leo.dai@emory.edu>
2023-09-24 23:04:02 -0400

    Updated MapManager.cs so that the player and the obstacle appears at the middle of the camera, and appears larger in the camers

--- Assets/Scripts/Game/MapManager.cs
+++ Assets/Scripts/Game/MapManager.cs
@@ -5,12 +5,14 @@ public class MapManager : MonoBehaviour
 {
     public static MapManager instance;
 
-    // Controls the width and height of the map
+    // Set the width, height and color of the map
     [SerializeField] private int width = 80, height = 45;
     [SerializeField] private Color32 darkColor = new Color32(0, 0, 0, 0), lightColor = new Color32(255, 255, 255, 255);
+    // Define the floor and wall tiles, and the floor and obstacle tilemaps
     [SerializeField] private TileBase floorTile, wallTile;
     [SerializeField] private Tilemap floorMap, obstacleMap;
 
+    // Getter methods
     public Tilemap FloorMap { get => floorMap; }
     public Tilemap ObstacleMap { get => obstacleMap; }
 
@@ -25,10 +27,13 @@ public class MapManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Set the center tile
         Vector3Int centerTile = new Vector3Int(width / 2, height / 2, 0);
 
+        // Set the wall bound to be located at x = 1, y = 1, with a width of 3 and a height of 1
         BoundsInt wallBounds = new BoundsInt(new Vector3Int(1, 1, 0), new Vector3Int(3, 1, 0));
 
+        // Iterately set the obstacle tiles
         for (int x = 0; x < wallBounds.size.x; x++)
         {
             for (int y = 0; y < wallBounds.size.y; y++)
@@ -39,9 +44,10 @@ public class MapManager : MonoBehaviour
         }
 
 
-        Camera.main.transform.position = new Vector3(40, 20.25f, -10);
-        Camera.main.orthographicSize = 27;
+        // Set the scale of the camera
+        Camera.main.orthographicSize = 20;
     }
 
+    // Used to check if the player is in bounds of the map
     public bool InBounds(int x, int y) => 0 <= x && x < width && 0 <= y && y < height;
 }
