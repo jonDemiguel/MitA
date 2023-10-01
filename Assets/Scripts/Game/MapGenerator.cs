c72c91a4eabda447a88d928f4cdd4d88e96e5f3b
Guanlin Dai <Leo.dai@emory.edu>
2023-10-01 15:40:20 -0400

    Created Noise Map

--- Assets/Scripts/Game/MapGenerator.cs
+++ Assets/Scripts/Game/MapGenerator.cs
@@ -7,9 +7,13 @@ public class MapGenerator : MonoBehaviour
 {
     public int mapWidth, mapHeight;
     public float noiseScale;
+    public bool autoUpdate;
 
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawnNoiseMap(noiseMap);
     }
 }
