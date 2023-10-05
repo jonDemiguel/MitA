497f61675c4cd28c4cc5bcbb4fcb81bf9f3fe00f
jdemiguel11 <jon.demiguel@emory.edu>
2023-10-05 04:02:17 -0400

    Finalized algorithm still need End Round implem and offset characters correctly

--- Assets/Scripts/Monsters/EnemySpawnManager.cs
+++ Assets/Scripts/Monsters/EnemySpawnManager.cs
@@ -7,20 +7,24 @@ public class EnemySpawnManager : MonoBehaviour
 {
     public static EnemySpawnManager Instance;
 
-    public List<GameObject> enemyTypes = new List<GameObject>();
-
-    public float spawnArea = 1.5f;
-
-    public float timeBetweenWaves = 15.0f; // give you 15 sec for default
-    public float timeBetweenSpawns = 0.5f; // half sec between enemy spawns
-    public int wave = 0; // start at wave 1
-    public int startEnemyCount = 3; // number of enemies... our base amount
-    public double modifier = 1.8171;
-    //public GameObject[] enemySpawnSystem;
-    public GameObject prefab;
-    private int enemyCount = 0; // active enemies
-    private int enemySpawned = 0;
     
+    //Time Offsets
+    public float timeBetweenWaves = 100.0f;
+    public float timeBetweenSpawns = 1.5f;
+
+    //Num enemy attributes
+    public int wave;
+    public int startEnemyCount = 3;
+    public double modifier = 1.8171;
+    private int enemyCount = 0;
+    private int enemySpawned = 0;
+    bool stillEnemies;
+
+    //In game assets
+    public GameObject[] prefab;
+    int screenWidth = Screen.width;
+    int screenHeight = Screen.height;
+    public Camera mainCamera;
 
     //Called before game start
     private void Awake()
@@ -31,52 +35,112 @@ public class EnemySpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        wave = 0;
         StartCoroutine(SpawnEnemies());
     }
 
     //Invokes new wave
-    public void StartWave(int numWave)
+    public void StartWave()
     {
+        //ADD INCREASED DIFICULTY OR OTHER ATTRIBUTES HERE
         InvokeRepeating("SpawnEnemies", timeBetweenSpawns, timeBetweenSpawns);
     }
-     
+
     private bool CheckEnemyCount()
     {
+        Debug.Log(startEnemyCount * Math.Pow(modifier, wave) + "            " + wave);
         if (enemySpawned >= (startEnemyCount * Math.Pow(modifier, wave)))
         {
-            return true;
+            return false;
         }
-        return false;
-
+        return true;
     }
 
     //End wave
     void EndWave()
     {
+        while(stillEnemies)
+        {
+            //Figure out how to monitor if Enemies have been destroyed
+            //
+        }
         wave++;
         enemyCount = 0;
         enemySpawned = 0;
-        Invoke("StartWave", timeBetweenWaves);
+        StartWave();
+    }
+
+    public Vector3 getSpawnLocation()
+    {
+        System.Random rnd = new System.Random();
+
+        //Camera dimensions
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+        float halfWidth = cameraWidth / 2f;
+        float halfHeight = cameraHeight / 2f;
+        
+        //Distance from edge of screen to monster
+        float marginOffset = 5f;
+
+        //Camera Location
+        Vector3 cameraLoc = mainCamera.transform.position;
+        Vector2 cameraPosition = new Vector2(cameraLoc.x, cameraLoc.y);
+        
+        //Random Angle to shoot from middle of scene
+        int randAngle = rnd.Next(0, 360);
+        Vector2 direction = new Vector2(Mathf.Cos(randAngle), Mathf.Sin(randAngle));
+
+        //Intersection point from random angle to border of on camera scene
+        Vector2 intersection = cameraPosition + new Vector2(direction.x * halfWidth, direction.y * halfHeight);
+        Vector3 intersectionPoint = new Vector3(intersection.x, intersection.y, 0f);
+
+        //Calculate to determine what border of screen intersection meets
+        Vector2 leftIntersection = cameraPosition + new Vector2(-halfWidth, -halfHeight) / direction;
+        Vector2 rightIntersection = cameraPosition + new Vector2(halfWidth, halfHeight) / direction;
+        Vector2 topIntersection = cameraPosition + new Vector2(-halfWidth, halfHeight) / direction;
+        Vector2 bottomIntersection = cameraPosition + new Vector2(halfWidth, -halfHeight) / direction;
+
+        //Offset so that sprite instantiates off camera by margin offset value
+        //If Right
+        if (direction.x > 0 && direction.y >= 0)
+        {
+            intersectionPoint.x += marginOffset;
+        }
+        //If left
+        else if (direction.x < 0 && direction.y >= 0)
+        {
+            intersectionPoint.x -= marginOffset;
+        }
+        //If top
+        else if (direction.x >= 0 && direction.y > 0)
+        {
+            intersectionPoint.y += marginOffset;
+        }
+        //If bottom
+        else if (direction.x >= 0 && direction.y < 0)
+        {
+            intersectionPoint.y -= marginOffset;
+        }
+
+        return intersectionPoint;
     }
 
     public IEnumerator SpawnEnemies()
     {
         while (CheckEnemyCount())
         {
+            
             yield return new WaitForSeconds(timeBetweenSpawns);
             System.Random rnd = new System.Random();
-            //int type = rnd.Next(0, enemySpawnSystem.Length);
-            //if(type == 0)
-            /*    Instantiate(Resources.Load<GameObject>("red_eye_monster")).name = "red_eye_monster";*/
-            //  if (type == 1)
-            //Instantiate(Resources.Load<GameObject>("Purple_Ghost_Monster")).name = "Purple_Ghost_Monster";
-            //            Instantiate(enemySpawnSystem[type]);
-
-            Instantiate(prefab, new Vector3(0, 0, 0f), transform.rotation);
-
-            //enemy.name = "Enemy" + i;
+            int type = rnd.Next(0, prefab.Length);
+            Vector3 spawnPosition = getSpawnLocation();
+            Vector3 spawnScale = new Vector3(10f, 10f, 1f);
+            Instantiate(prefab[type], spawnPosition, transform.rotation);
+            prefab[type].transform.localScale = spawnScale;
             enemySpawned++;
-            enemyCount++; // add an enemy when it's spawned
+            enemyCount++;
         }
+        EndWave();
     }
 }
