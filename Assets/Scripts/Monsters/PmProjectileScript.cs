ad477089b36156e96ab139b140eb9023d612614f
Frederic Guintu <frederic.guintu@emory.edu>
2023-10-05 20:40:54 -0400

    moved script files to right folders

--- Assets/Scripts/Monsters/PmProjectileScript.cs
+++ Assets/Scripts/Monsters/PmProjectileScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PmProjectileScript : MonoBehaviour
 {   
 
     public GameObject player;
+    public Collider2D playerbox;
     private Rigidbody2D rb;
     public float speed;
     private float timer;
@@ -15,8 +17,10 @@ public class PmProjectileScript : MonoBehaviour
         
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
+        playerbox = player.GetComponent<Collider2D>();
+        Vector3 targetPosition = playerbox.bounds.center;
         if (player == null ) Destroy(this.gameObject);
-        Vector3 moveDirection = player.transform.position - transform.position;
+        Vector3 moveDirection = targetPosition - transform.position;
         float v = (float)(moveDirection.y + .09);
         rb.velocity = new Vector2(moveDirection.x, v).normalized * speed;
 
