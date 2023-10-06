85d6fc57fb3f20d81b74254f0eeb979694e28502
Frederic Guintu <frederic.guintu@emory.edu>
2023-10-06 15:54:31 -0400

    made a new script CMAB that makes movement/animated sprite better. finalized the enemy->character dmg prefabs into prefab folder. added weapons in assets. WIP: character->enemy attack system

---
+++ Assets/Scripts/Character/CharacterMovementAnimationBased.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMAB : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Vector2 movement;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Get horizontal and vertical inputs
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveX, moveY).normalized;
+
+        // Set animator parameters
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(movement * speed * Time.fixedDeltaTime);
+    }
+}
