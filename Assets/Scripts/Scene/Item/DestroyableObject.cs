b84d53b05b3d144a0a5195233f28b959677fe831
Tianyi Zhang <zhangtianyi975@gmail.com>
2023-10-28 12:07:01 -0400

    add destroyable props<F5>

--- Assets/Scripts/Scene/Item/DestroyableObject.cs
+++ Assets/Scripts/Scene/Item/DestroyableObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyableObject : MonoBehaviour
+public class DestroyableObject : MonoBehaviour 
 {
     // Start is called before the first frame update
     void Start()
