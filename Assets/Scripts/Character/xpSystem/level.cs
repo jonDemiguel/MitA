82dc30d619a337f8a1f2ea5aea5aa3d70cb59a90
Andy Han <xhan68@emory.edu>
2023-11-03 10:25:19 -0400

    Added new changess

--- Assets/Scripts/Character/xpSystem/level.cs
+++ Assets/Scripts/Character/xpSystem/level.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
@@ -58,7 +59,10 @@ public class Level : MonoBehaviour
                 upGradeAttribute();
 
         }
-        
+        if(level == 10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     private void OnLevelUp()
