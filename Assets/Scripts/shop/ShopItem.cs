54404c5a7266ecd849099c9a446dba6ce6a9bf95
Eric Juarez <ericjuarez123@gmail.com>
2023-11-05 10:42:46 -0500

    Updated ShopItem

--- Assets/Scripts/shop/ShopItem.cs
+++ Assets/Scripts/shop/ShopItem.cs
@@ -6,5 +6,5 @@ public class ShopItem : MonoBehaviour
 {
     public string itemName;
     public int price;
-    public int stock;  // New field to represent the number of items in stock
+    public int stock;  
 }
