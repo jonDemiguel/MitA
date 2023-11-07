fe9539e4c206a657e9ded7ff48fe109814992a8b
jdemiguel11 <jon.demiguel@emory.edu>
2023-11-07 02:16:19 -0500

    Round structure implemented still bugs relating to monster, Loading after Level2 scene, level 3 prop instantiation, need to add level up UI between levels

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager gameManager {  get; private set; }
+    public static GameManager gameManager { get; private set; }
 
+    public string sceneName;
+    public int enemiesKilled = 0;
     public UnitHealth _playerHealth = new UnitHealth(100, 100);
 
     private void Awake()
@@ -19,5 +23,30 @@ public class GameManager : MonoBehaviour
             gameManager = this;
         }
     }
+
+
+    public void AddKillCount()
+    {
+        enemiesKilled++;
+    }
+
+    public void ResetKillCount()
+    {
+        enemiesKilled = 0;
+    }
+
+    public string GetActiveScene()
+    {
+        return sceneName;
+    }
+
+    public void NewScene()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+   public int GetKillCount()
+   {
+        return enemiesKilled;
+   }
 
 }
